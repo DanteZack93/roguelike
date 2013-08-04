6b294106809b052cca2eb8996e74639bc1f0eaaf
DanteZack93 <hasan.xhaferri@gmail.com>
2013-08-04 13:39:41 +0200

    Testing the website editing on the main.cpp
    Testing if changing the main in the website affects the local project

--- RogueLike/main.cpp
+++ RogueLike/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    //Let's see if this works
+    //Let's see if this works(made on the PC)
+    //Chenge made on the webiste
     return 0;
 }
